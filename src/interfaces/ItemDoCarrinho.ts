3026f22b4c9b5d1d0b1ec156d9d124fd0a81f09c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-21 19:34:45 -0300

    ✨ feat: cria interface ItemDoCarrinho

---
+++ src/interfaces/ItemDoCarrinho.ts
@@ -0,0 +1,10 @@
+import { OpcaoCompra } from '@/interfaces/OpcaoCompra'
+import { Livro } from '@/interfaces/Livro'
+
+export interface ItemDoCarrinho {
+    quantidade: number
+    livroId: number
+    opcaoCompraId: number
+    opcaoCompra: OpcaoCompra
+    livro: Livro
+}
