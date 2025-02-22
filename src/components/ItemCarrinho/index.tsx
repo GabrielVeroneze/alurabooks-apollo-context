c5a813abe714a98ca7736f49004a8098432a5f5b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-21 23:31:09 -0300

    ✨ feat: cria componente ItemCarrinho

---
+++ src/components/ItemCarrinho/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { AbInputQuantidade } from 'ds-alurabooks'
+import { formatador } from '@/utils/formatador-moeda'
+import { ItemDoCarrinho } from '@/interfaces/ItemDoCarrinho'
+import lixeira from './assets/lixeira.svg'
+import styles from './ItemCarrinho.module.scss'
+
+interface ItemCarrinhoProps {
+    item: ItemDoCarrinho
+}
+
+const ItemCarrinho = ({ item }: ItemCarrinhoProps) => {
+    const [quantidade, setQuantidade] = useState<number>(item.quantidade)
+
+    return (
+        <li className={styles.item}>
+            <div className={styles.info}>
+                <img
+                    className={styles.imagem}
+                    src={item.livro.imagemCapa}
+                    alt={item.livro.descricao}
+                />
+                <h3 className={styles.titulo}>{item.livro.titulo}</h3>
+                <p className={styles.descricao}>{item.livro.descricao}</p>
+                <p className={styles.autor}>Por: {item.livro.autor.nome}</p>
+            </div>
+            <div className={styles.compra}>
+                <div className={styles.preco}>
+                    <h4 className={styles.rotulo}>Preço</h4>
+                    <p className={styles.valor}>
+                        {formatador.format(item.opcaoCompra.preco)}
+                    </p>
+                </div>
+                <AbInputQuantidade
+                    value={quantidade}
+                    onChange={setQuantidade}
+                />
+                <button className={styles.botao}>
+                    <img src={lixeira} alt="Ícone de uma lixeira" />
+                </button>
+            </div>
+        </li>
+    )
+}
+
+export default ItemCarrinho
