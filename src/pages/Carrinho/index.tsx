296364fe320dc9923a0966a9ea9bdf8e8a14254e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-22 16:59:56 -0300

    ✨ feat: melhora estrutura e adicionar título principal

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { AbBotao } from 'ds-alurabooks'
 import { useCarrinho } from '@/graphql/carrinho/hooks'
 import { formatador } from '@/utils/formatador-moeda'
+import TituloPrincipal from '@/components/TituloPrincipal'
 import ItemCarrinho from '@/components/ItemCarrinho'
 import styles from './Carrinho.module.scss'
 
@@ -9,25 +10,28 @@ const Carrinho = () => {
     const { data } = useCarrinho()
 
     return (
-        <section className={styles.secao}>
-            <h2 className={styles.titulo}>Itens selecionados</h2>
-            <ul className={styles.lista}>
-                {data?.carrinho.itens.map(item => (
-                    <ItemCarrinho key={item.livro.id} item={item} />
-                ))}
-            </ul>
-            <Link className={styles.link} to="#">
-                Continuar comprando
-            </Link>
-            <hr />
-            <div className={styles.totalCompra}>
-                <p className={styles.textoTotal}>
-                    Total da compra{' '}
-                    <strong className={styles.valorTotal}>
-                        {formatador.format(data?.carrinho.total || 0)}
-                    </strong>
-                </p>
-                <AbBotao texto="Finalizar compra" />
+        <section>
+            <TituloPrincipal texto="Minha sacola" />
+            <div className={styles.conteudo}>
+                <h2 className={styles.titulo}>Itens selecionados</h2>
+                <ul className={styles.lista}>
+                    {data?.carrinho.itens.map(item => (
+                        <ItemCarrinho key={item.livro.id} item={item} />
+                    ))}
+                </ul>
+                <Link className={styles.link} to="/">
+                    Continuar comprando
+                </Link>
+                <hr />
+                <div className={styles.totalCompra}>
+                    <p className={styles.textoTotal}>
+                        Total da compra{' '}
+                        <strong className={styles.valorTotal}>
+                            {formatador.format(data?.carrinho.total || 0)}
+                        </strong>
+                    </p>
+                    <AbBotao texto="Finalizar compra" />
+                </div>
             </div>
         </section>
     )
