f0a1dc516338cc83db5b71c1b2fe7116c4541cb0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-25 20:25:57 -0300

    ♻️ refactor: extrai código para componente CategoriaMenu

---
+++ src/components/BarraNavegacao/CategoriaMenu/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import { CategoriaDados } from '@/interfaces/CategoriaDados'
+import styles from './CategoriaMenu.module.scss'
+
+interface CategoriaMenuProps {
+    categoria: CategoriaDados
+}
+
+const CategoriaMenu = ({ categoria }: CategoriaMenuProps) => {
+    return (
+        <div key={categoria.id} className={styles.opcao}>
+            <Link className={styles.link} to={`/categorias/${categoria.slug}`}>
+                {categoria.nome}
+            </Link>
+        </div>
+    )
+}
+
+export default CategoriaMenu
