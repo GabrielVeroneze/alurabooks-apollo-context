b283d709e5e570a0fa7f0c7b084a6765200d7ee6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-26 18:49:17 -0300

    ♻️ refactor: torna `onClick` opcional no componente `BotaoIcone`

--- src/components/BotaoIcone/index.tsx
+++ src/components/BotaoIcone/index.tsx
@@ -4,12 +4,12 @@ interface BotaoIconeProps {
     texto: string
     imagem: string
     imagemAlt: string
-    onClick: () => void
+    onClick?: () => void
 }
 
 const BotaoIcone = ({ texto, imagem, imagemAlt, onClick }: BotaoIconeProps) => {
     return (
-        <button className={styles.botao} onClick={() => onClick()}>
+        <button className={styles.botao} onClick={() => onClick && onClick()}>
             <img src={imagem} alt={imagemAlt} />
             {texto}
         </button>
